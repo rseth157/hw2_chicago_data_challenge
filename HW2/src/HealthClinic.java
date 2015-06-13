2427b50298c8efc8e8d411de36dacb08e4b34b10
Richa Seth <rseth@luc.edu>
2015-06-13 17:15:50 -0500

    Adding class to store health clinic data file

---
+++ HW2/src/HealthClinic.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ * @author Richa
+ *
+ */
+public class HealthClinic {
+	String facility;
+	String community;
+	String phone;
+	String description;
+	String address;
+	
+	/**
+	 * 
+	 * @return facility
+	 */
+	public String getFacilty() {
+		return facility;
+	}
+	
+	/**
+	 * 
+	 * @param facility
+	 */
+	public void setFacilty(String facilty) {
+		this.facility = facilty;
+	}
+	
+	/**
+	 * 
+	 * @return community
+	 */
+	public String getCommunity() {
+		return community;
+	}
+	
+	/**
+	 * 
+	 * @param community
+	 */
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+	
+	/**
+	 * 
+	 * @return phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	
+	/**
+	 * 
+	 * @param phone
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	/**
+	 * 
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * 
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * @return address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * 
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
