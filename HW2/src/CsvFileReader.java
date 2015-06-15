6ac8b6a581bc0d535f462cbdfd8b09662df98ef1
Richa Seth <rseth@luc.edu>
2015-06-15 17:33:04 -0500

    Final code for CSV file reader

--- HW2/src/CsvFileReader.java
+++ HW2/src/CsvFileReader.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,26 +18,28 @@ public class CsvFileReader {
 	private static final int COMMUNITY_IDX = 1;
 	private static final int DESCRIPTION_IDX = 3;
 	private static final int PHONE_IDX = 2;
-	//private static final int ADDRESS_IDX = 4;
+	// private static final int ADDRESS_IDX = 4;
 	private static final int COMMUNITYID_IDX = 0;
 	private static final int COMMUNITYNAME_IDX = 1;
 	private static final int LIFE1999_IDX = 2;
-	private static final int LIFE1999L_IDX = 3;
-	private static final int LIFE1999UP_IDX = 4;
+
 	private static final int LIFE2000_IDX = 5;
-	private static final int LIFE2000L_IDX = 6;
-	private static final int LIFE2000UP_IDX = 7;
+
 	private static final int LIFE2010_IDX = 8;
-	private static final int LIFE2010L_IDX = 9;
-	private static final int LIFE2010UP_IDX = 5;
-	
-	public static void readClinicCsvFile(String fileName) {
+
+	/**
+	 * Reads the health clinic file and store the data into a list
+	 * @param fileName
+	 * @param Clinics
+	 */
+	public static void readClinicCsvFile(String fileName,
+			List<HealthClinic> Clinics) {
 
 		BufferedReader fileReader = null;
 		try {
 
 			// Create a new list of clinics to be filled by CSV file data
-			List<HealthClinic> Clinics = new ArrayList<HealthClinic>();
+			// List<HealthClinic> Clinics = new ArrayList<HealthClinic>();
 
 			String line = "";
 
@@ -51,24 +52,18 @@ public class CsvFileReader {
 			// Read the file line by line starting from the second line
 			while ((line = fileReader.readLine()) != null) {
 				// Get all tokens available in line
-				String[] tokens = line.split(COMMA_DELIMITER);
-				if (tokens.length > 0) {
+				String[] data = line.split(COMMA_DELIMITER);
+				if (data.length > 0) {
 					// Create a new Clinic object and fill data
 					HealthClinic clinic = new HealthClinic(
-							tokens[FACILITY_IDX], tokens[COMMUNITY_IDX],
-							tokens[PHONE_IDX], tokens[DESCRIPTION_IDX]/*
+							data[FACILITY_IDX], data[COMMUNITY_IDX],
+							data[PHONE_IDX], data[DESCRIPTION_IDX]/*
 																	 * , tokens[
 																	 * ADDRESS_IDX
 																	 * ]
 																	 */);
 					Clinics.add(clinic);
 				}
-			}
-
-			System.out.println("total records = " + Clinics.size());
-			// Print the new clinic list
-			for (HealthClinic clinic : Clinics) {
-				System.out.println(clinic.toString());
 			}
 
 		} catch (Exception e) {
@@ -84,14 +79,22 @@ public class CsvFileReader {
 		}
 
 	}
-	
-	public static void readLifeCsvFile(String fileName) {
+
+	/**
+	 * Reads the Life Expectency file and store the data into a list
+	 * Also calculate the number of clinics in each community from clinic list and save it
+	 * @param fileName
+	 * @param Clinics
+	 * @param lifeExp
+	 */
+	public static void readLifeCsvFile(String fileName,
+			List<HealthClinic> Clinics, List<LifeExpectancy> lifeExp) {
 
 		BufferedReader fileReader = null;
 		try {
 
-			// Create a new list of clinics to be filled by CSV file data
-			List<LifeExpectancy> lifeExp = new ArrayList<LifeExpectancy>();
+			// Create a new list of data to be filled by CSV file data
+			// List<LifeExpectancy> lifeExp = new ArrayList<LifeExpectancy>();
 
 			String line = "";
 
@@ -104,20 +107,31 @@ public class CsvFileReader {
 			// Read the file line by line starting from the second line
 			while ((line = fileReader.readLine()) != null) {
 				// Get all tokens available in line
-				String[] tokens = line.split(COMMA_DELIMITER);
-				if (tokens.length > 0) {
-					// Create a new Clinic object and fill data
+				String[] data = line.split(COMMA_DELIMITER);
+				if (data.length > 0) {
+					// Create a new LifeExpectancy object and fill data
 					LifeExpectancy lifeE = new LifeExpectancy(
-							tokens[COMMUNITYNAME_IDX], Float.parseFloat(tokens[LIFE1999_IDX]),
-							Float.parseFloat(tokens[LIFE2000_IDX]), Float.parseFloat(tokens[LIFE2010_IDX]));
+							Integer.parseInt(data[COMMUNITYID_IDX].trim()),
+							data[COMMUNITYNAME_IDX],
+							Float.parseFloat(data[LIFE1999_IDX]),
+							Float.parseFloat(data[LIFE2000_IDX]),
+							Float.parseFloat(data[LIFE2010_IDX]));
 					lifeExp.add(lifeE);
 				}
 			}
 
-			System.out.println("total records = " + lifeExp.size());
-			// Print the new clinic list
+			// Adding number of clinics in each area
 			for (LifeExpectancy l : lifeExp) {
-				System.out.println(l.toString());
+				int clinicCount = 0;
+				for (HealthClinic clinic : Clinics) {
+					if (clinic.getCommunity().contains(
+							l.getCommunity().toUpperCase())
+							&& clinic.getCommunity().contains(
+									String.valueOf(l.getCommunityNumber()))) {
+						clinicCount++;
+					}
+				}
+				l.setNumberOfClinics(clinicCount);
 			}
 
 		} catch (Exception e) {
